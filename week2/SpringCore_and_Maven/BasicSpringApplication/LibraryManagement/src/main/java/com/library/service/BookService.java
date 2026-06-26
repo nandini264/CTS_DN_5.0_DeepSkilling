665133c85242f6892664034921425fdd864795d0
nandini264 <nandinipalukuri462@gmail.com>
2026-06-27 00:09:28 +0530

    Added Week 2 Spring exercises

---
+++ week2/SpringCore_and_Maven/BasicSpringApplication/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class BookService {
+
+    public void showService() {
+        System.out.println("Book Service Working");
+    }
+}
---
+++ week2/SpringCore_and_Maven/DependancyInjection/LibraryManagement/src/main/java/com/library/LibraryManagement/LibraryApplication.java
@@ -0,0 +1,19 @@
+package com.library.LibraryManagement;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service=context.getBean("bookService", BookService.class);
+        service.showService();
+        
+//        BookRepository repo=context.getBean("bookRepository", BookRepository.class);
+//        repo.display();
+    }
+}
